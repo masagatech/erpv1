e64a5be8473fe83d0e619b02e0dee11f70aacc58
vivekpandey12 <vivek.pandey5188@gmail.com>
2016-12-21 20:13:46 +0530

    pipe added

--- src/app/_pipe/groupby.pipe.ts
+++ src/app/_pipe/groupby.pipe.ts
@@ -1,10 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core'
 
-@Pipe({name: 'groupBy'})
+@Pipe({
+  name: 'groupBy'
+})
 export class GroupByPipe implements PipeTransform {
   transform(value: Array<any>, field: string): Array<any> {
-    const groupedObj = value.reduce((prev, cur)=> {
-      if(!prev[cur[field]]) {
+    const groupedObj = value.reduce((prev, cur) => {
+      if (!prev[cur[field]]) {
         prev[cur[field]] = [cur];
       } else {
         prev[cur[field]].push(cur);
---
+++ src/app/_pipe/orderby.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe } from "angular2/core";
+import { Todo } from './todo';
+
+@Pipe({
+  name: "orderBy"
+})
+export class OrderByPipe {
+  transform(array: Array<any>, args: string): Array<any> {
+    array.sort((a: any, b: any) => {
+      if (a < b) {
+        return -1;
+      } else if (a > b) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    return array;
+  }
+}
