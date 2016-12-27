b126c805608765705b83afa8ae936106dfc9c1e7
vivekpandey12 <vivek.pandey5188@gmail.com>
2016-12-27 11:16:26 +0530

    make new form master of master

---
+++ src/app/modules/setting/mom/aded/addmom.comp.ts
@@ -0,0 +1,151 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SharedVariableService } from "../../../../_service/sharedvariable-service";
+import { ActionBtnProp } from '../../../../_model/action_buttons';
+import { Subscription } from 'rxjs/Subscription';
+import { CommonService } from '../../../../_service/common/common-service' /* add reference for MOM */
+import { Router, ActivatedRoute } from '@angular/router';
+import { MessageService, messageType } from '../../../../_service/messages/message-service';
+
+declare var $: any;
+
+@Component({
+    templateUrl: 'addmom.comp.html',
+    providers: [CommonService]
+})
+
+export class AddMOM implements OnInit, OnDestroy {
+    title: any;
+    validSuccess: Boolean = true;
+
+    momid: number = 0;
+    group: string = "";
+    key: string = "";
+    val: string = "";
+
+    actionButton: ActionBtnProp[] = [];
+    subscr_actionbarevt: Subscription;
+
+    private subscribeParameters: any;
+
+    constructor(private _routeParams: ActivatedRoute, private _router: Router, private setActionButtons: SharedVariableService,
+        private _commonservice: CommonService, private _message: MessageService) {
+
+    }
+
+    uploadedFiles: any[] = [];
+
+    onUpload(event) {
+        for (let file of event.files) {
+            this.uploadedFiles.push(file);
+        }
+    }
+    
+    saveMOMDetails() {
+        var that = this;
+        debugger;
+
+        var saveMOM = {
+            "id": that.momid,
+            "group": that.group,
+            "key": that.key,
+            "val": that.val,
+            "createdby": "1:vivek",
+            "updatedby": "1:vivek"
+        }
+
+        if (that.validSuccess) {
+            that._commonservice.saveMOM(saveMOM).subscribe(data => {
+                var dataResult = data.data;
+                console.log(dataResult);
+
+                if (dataResult[0].funsave_mom.doc == "1") {
+                    //alert(dataResult[0].funsave_mom.msg);
+                    that._message.Show(messageType.success, 'Confirmed', dataResult[0].funsave_mom.msg.toString());
+                    that._router.navigate(['/setting/masterofmaster']);
+                }
+                else if (dataResult[0].funsave_mom.doc == "-1") {
+                    that._message.Show(messageType.info, 'Info', dataResult[0].funsave_mom.msg.toString());
+                }
+                else {
+                    that._message.Show(messageType.error, 'Error', "Error");
+                }
+            }, err => {
+                that._message.Show(messageType.error, 'Error', err);
+            }, () => {
+                // console.log("Complete");
+            });
+        }
+    }
+
+    // get mom by id
+
+    getMOMByID(pmomid: number) {
+        this._commonservice.getMOM({ "flag": "id", "id": pmomid }).subscribe(data => {
+            var dataresult = data.data;
+
+            this.momid = dataresult[0].id;
+            this.group = dataresult[0].group;
+            this.key = dataresult[0].key;
+            this.val = dataresult[0].val;
+        }, err => {
+            console.log("Error");
+        }, () => {
+            // console.log("Complete");
+        })
+    }
+
+    actionBarEvt(evt) {
+        if (evt === "save") {
+            this._message.confirm('Are you sure that you want to save?', () => {
+                this.saveMOMDetails()
+            });
+        } else if (evt === "edit") {
+            $('#Group').attr('disabled', 'disabled');
+            $('#Key').attr('disabled', 'disabled');
+            $('#Val').removeAttr('disabled');
+
+            this.actionButton.find(a => a.id === "save").hide = false;
+            this.actionButton.find(a => a.id === "edit").hide = true;
+        } else if (evt === "delete") {
+            alert("delete called");
+        }
+    }
+
+    ngOnInit() {
+        this.actionButton.push(new ActionBtnProp("save", "Save", "save", true, false));
+        this.actionButton.push(new ActionBtnProp("edit", "Edit", "edit", true, false));
+        this.actionButton.push(new ActionBtnProp("delete", "Delete", "trash", true, false));
+
+        this.setActionButtons.setActionButtons(this.actionButton);
+        this.subscr_actionbarevt = this.setActionButtons.setActionButtonsEvent$.subscribe(evt => this.actionBarEvt(evt));
+
+        this.subscribeParameters = this._routeParams.params.subscribe(params => {
+            if (params['id'] !== undefined) {
+                this.actionButton.find(a => a.id === "save").hide = true;
+                this.actionButton.find(a => a.id === "edit").hide = false;
+
+                this.momid = params['id'];
+                this.getMOMByID(this.momid);
+
+                $('#Group').attr('disabled', 'disabled');
+                $('#Key').attr('disabled', 'disabled');
+                $('#Val').attr('disabled', 'disabled');
+            }
+            else {
+                this.actionButton.find(a => a.id === "save").hide = false;
+                this.actionButton.find(a => a.id === "edit").hide = true;
+
+                $('#Group').removeAttr('disabled');
+                $('#Group').removeAttr('disabled');
+                $('#Val').removeAttr('disabled');
+            }
+        });
+    }
+
+    ngOnDestroy() {
+        console.log('ngOnDestroy');
+        this.actionButton = [];
+        this.subscr_actionbarevt.unsubscribe();
+        this.subscribeParameters.unsubscribe();
+    }
+}
---
+++ src/app/modules/setting/mom/view/viewmom.comp.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SharedVariableService } from "../../../../_service/sharedvariable-service";
+import { ActionBtnProp } from '../../../../_model/action_buttons';
+import { Subscription } from 'rxjs/Subscription';
+import { CommonService } from '../../../../_service/common/common-service' /* add reference for MOM */
+import { Router } from '@angular/router';
+import { LazyLoadEvent, DataTable } from 'primeng/primeng';
+
+@Component({
+    templateUrl: 'viewMOM.comp.html',
+    providers: [CommonService]
+})
+
+export class ViewMOM implements OnInit, OnDestroy {
+    actionButton: ActionBtnProp[] = [];
+    subscr_actionbarevt: Subscription;
+
+    datasource: any = [];
+    mom: any = [];
+    totalRecords: number = 0;
+    selectedMOM1: any = [];
+
+    constructor(private _router: Router, private setActionButtons: SharedVariableService, private _commonservice: CommonService) {
+
+    }
+
+    BindMOMGrid(from: number, to: number) {
+        var that = this;
+        that._commonservice.getMOMGrid({ "flag": "grid", "from": from, "to": to }).subscribe(data => {
+            that.totalRecords = data.data[1][0].recordstotal;
+            that.mom = data.data[0];
+        });
+    }
+
+    loadMOMGrid(event: LazyLoadEvent) {
+        this.BindMOMGrid(event.first, (event.first + event.rows));
+    }
+
+    openMOMDetails(row) {
+        this._router.navigate(["/setting/masterofmaster/edit", row.id]);
+    }
+
+    ngOnInit() {
+        this.actionButton.push(new ActionBtnProp("add", "Add", "plus", true, false));
+        this.setActionButtons.setActionButtons(this.actionButton);
+        this.subscr_actionbarevt = this.setActionButtons.setActionButtonsEvent$.subscribe(evt => this.actionBarEvt(evt));
+    }
+
+    actionBarEvt(evt) {
+        if (evt === "add") {
+            this._router.navigate(['/setting/masterofmaster/add']);
+        }
+    }
+
+    ngOnDestroy() {
+        console.log('ngOnDestroy');
+        this.subscr_actionbarevt.unsubscribe();
+    }
+}
