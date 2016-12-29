768af82fe5ad1b7d402e9b731ae130e1cc19c910
zaidansari <azaid51@gmail.com>
2016-12-29 06:10:51 -0800

    Address Usercontroll

--- src/app/modules/usercontrol/addressbook/adrbook.comp.ts
+++ src/app/modules/usercontrol/addressbook/adrbook.comp.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter, OnDestroy, ViewChild } from '@angular/core';
 import { AdrBookService } from '../../../_service/addressbook/adrbook-service' //Add Address Book service
+import { CommonService } from '../../../_service/common/common-service' /* add reference for master of master */
 
 declare var $: any;
 
@@ -11,29 +12,55 @@ declare var $: any;
 
 export class AddrbookComp implements OnInit, OnDestroy {
 
-    @Input() mod: string = "";
-    @Input() adr1: any = "";
-    @Input() adr2: any = "";
-    @Input() mob: any = 0;
-    @Input() pin: any = "";
-    @Input() altmob: any = "";
-    @Input() city: any = 0;
-    @Input() state: any = 0;
-    @Input() country: any = 0;
-    @Input() adrtype: any = 0;
-    @Input() landmark: any = "";
+    adrbookid: any = 0;
+    mod: string = "";
+    adr1: any = "";
+    adr2: any = "";
+    mob: any = 0;
+    pin: any = "";
+    altmob: any = "";
+    email: any = "";
+    otheremail: any = "";
+    city: any = "";
+    state: any = "";
+    country: any = 0;
+    adrtype: any = 0;
+    landmark: any = "";
+    remark: any = "";
     addrbooklist: any = [];
+    countryDT: any[];
+    chkprimary: boolean = false;
+    chkpri:boolean=false;
 
-
-    constructor(private _adrbookservice: AdrBookService) {
+    constructor(private _adrbookservice: AdrBookService, private _commonservice: CommonService) {
 
         this.getAddress();
     }
 
+    filldropdown() {
+        var that = this;
+        this._commonservice.getMOM({ "group": "Country" }).subscribe(data => {
+            that.countryDT = data.data;
+        }, err => {
+            console.log("Error");
+        }, () => {
+            console.log("Complete");
+        })
+    }
+
+
+    AddBook() {
+        var that = this;
+        that.ClearControll();
+        that.filldropdown();
+        setTimeout(function () {
+            $(".addr1").focus();
+        }, 500);
+    }
 
     public getAddress() {
         var _this = this;
-        this._adrbookservice.getAdrBook({}).subscribe(result => {
+        this._adrbookservice.getAdrBook({ "cmpid": 1, "flag": "" }).subscribe(result => {
             _this.addrbooklist = result.data;
         }, err => {
             console.log("Error");
@@ -50,35 +77,79 @@ export class AddrbookComp implements OnInit, OnDestroy {
         that.pin = "";
         that.altmob = "";
         that.city = "";
+        that.email = "";
+        that.otheremail = "";
         that.state = "";
         that.country = "";
         that.adrtype = "";
         that.landmark = "";
+        that.remark="";
         that.mod = "";
+    }
+
+    EditAdr(row) {
+        var _this = this;
+        _this.filldropdown();
+        this._adrbookservice.getAdrBook({
+            "cmpid": 1,
+            "adrid": row.id,
+            "flag": "edit"
+        }).subscribe(result => {
+            var dataset = result.data;
+            _this.adrbookid = dataset[0].id;
+            _this.adr1 = dataset[0].address1;
+            _this.adr2 = dataset[0].address2;
+            _this.mob = dataset[0].mob;
+            _this.altmob = dataset[0].othermob;
+            _this.email = dataset[0].email;
+            _this.otheremail = dataset[0].otheremail;
+            _this.pin = dataset[0].pincode;
+            _this.city = dataset[0].city;
+            _this.state = dataset[0].state;
+            _this.country = dataset[0].country;
+            _this.adrtype = dataset[0].adrtype;
+            _this.chkprimary = dataset[0].isprimary;
+            _this.landmark = dataset[0].landmark;
+            _this.chkpri=dataset[0].isprimary;
+            setTimeout(function () {
+                $(".addr1").focus();
+            }, 500);
+        }, err => {
+            console.log("Error");
+        }, () => {
+            // console.log("Complete");
+        })
+
+
     }
 
     Parameter() {
         var that = this;
         var Param = {
+            "adrid": that.adrbookid,
             "adr1": that.adr1,
             "adr2": that.adr2,
-            "mob": that.mob,
+            "mob": that.mob == "" ? 0 : that.mob,
             "pin": that.pin,
-            "altmob": { "altmob": "9321284093" },
+            "altmob": that.altmob,
             "cmpid": 1,
             "city": that.city,
             "state": that.state,
-            "country": that.country,
-            "adrtype": that.adrtype,
-            "landmark": that.landmark,
+            "email": that.email,
+            "otheremail": that.otheremail,
+            "country": that.country == "" ? 0 : that.country,
+            "adrtype": that.adrtype == "" ? 0 : that.adrtype,
+            "landmrk": that.landmark,
+            "chkprimary": that.chkprimary,
             "module": "attr",
             "entity": 1,
             "createdby": "admin",
-            "remark": "",
+            "remark": that.remark,
             "remark1": "",
             "remark2": "",
             "remark3": ""
         }
+        console.log(Param);
         return Param;
     }
 
@@ -92,7 +163,7 @@ export class AddrbookComp implements OnInit, OnDestroy {
                 alert("Data Save Siccessfully");
                 that.ClearControll();
                 that.getAddress();
-                return;
+                $(".addr1").focus();
             }
             console.log(dataset);
         }, err => {
@@ -103,11 +174,10 @@ export class AddrbookComp implements OnInit, OnDestroy {
     }
 
     ngOnInit() {
+
         setTimeout(function () {
 
-        }, 0);
-
-
+        }, 1);
     }
 
     ngOnDestroy() {
