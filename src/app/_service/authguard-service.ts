a4f382955ccb73dfc287bed0732e89158938dd39
pratikn <pratikway.90@gmail.com>
2017-01-24 16:59:05 +0530

    login hold session
    login hold session

--- src/app/_service/authguard-service.ts
+++ src/app/_service/authguard-service.ts
@@ -1,27 +1,79 @@
-import { Injectable,Inject }     from '@angular/core';
-import { CanActivate,CanLoad,CanActivateChild }    from '@angular/router';
+import { Injectable, Inject } from '@angular/core';
+import { CanActivate, CanLoad, CanActivateChild } from '@angular/router';
 import { AuthenticationService } from '../_service/auth-service';
-
+import { UserService } from '../_service/user/user-service'
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { Router } from '@angular/router';
 
 @Injectable()
-export class AuthGuard implements CanActivate, CanLoad ,CanActivateChild{
-  constructor(private authser: AuthenticationService){
+export class AuthGuard implements CanActivate, CanLoad, CanActivateChild {
+  constructor(private authser: AuthenticationService, private _userService: UserService, private _router: Router) {
 
   }
 
-  canActivate() {
+
+  canActivate(): Observable<boolean> {
+    return this.checkFun();
+  }
+
+  canActivateChild(): Observable<boolean> {
     //console.log('AuthGuard#canActivate called');
-    
-    return this.authser.checkCredentials();
+    return this.checkFun();
   }
 
-  canActivateChild() {
-    //console.log('AuthGuard#canActivate called');
-    return this.authser.checkCredentials();
-  }
-
-   canLoad() {
+  canLoad() {
     //console.log('AuthGuard#canLoad called');
-     return this.authser.checkCredentials();
+    return true;
+    //return this.authser.checkCredentials();
+  }
+
+
+  private checkFun(): Observable<boolean> {
+    var checks = this.authser.checkCredentials();
+    console.log(checks);
+    return Observable.create((observer: Subject<boolean>) => {
+      if (checks.status) {
+        observer.next(true);
+      } else if (checks.takefrmdb) {
+        this.authser.loginsession({ "base": "_sid", "sid": checks.sessionid }).subscribe(d => {
+          if (d) {
+            if (d.status) {
+              let usrobj = d.data;
+              let userDetails = usrobj[0];
+              if (userDetails.status) {
+                this._userService.setUsers(userDetails);
+                if (userDetails.cmpid != 0 && userDetails.fyid != 0) {
+                  // propr user
+                } else if (userDetails.errcode === "chpwd") {
+                  this._router.navigate(['/changepwd']);
+                } else {
+                  this._router.navigate(['/usersettings/defaultcompandfy']);
+                }
+              } else {
+                this._router.navigate(['login']);
+                console.log("user status false");
+              }
+            } else {
+              this._router.navigate(['login']);
+              console.log("data status false");
+            }
+          } else {
+            this._router.navigate(['login']);
+          }
+            observer.next(true);
+        }, err => {
+          this._router.navigate(['login']);
+          console.log(err);
+            observer.next(true);
+        });
+
+      } else {
+        this._router.navigate(['login']);
+        console.log("logged out");
+          observer.next(true);
+      }
+    
+    }).first();
   }
 }
--- src/app/_service/dataconnect.ts
+++ src/app/_service/dataconnect.ts
@@ -1,31 +1,35 @@
 import { Injectable } from '@angular/core';
-import { Http ,Headers, RequestOptions,Response} from '@angular/http';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { Globals } from '../../app/_const/globals';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-import {Observable} from 'rxjs/Rx';
+import { Observable } from 'rxjs/Rx';
 
 
 @Injectable()
 export class DataService {
-     
-     global = new Globals(); 
-     
-     constructor(private _http:Http ) { }
-    
-    post(api:string, params:any)
-    {
+
+    global = new Globals();
+
+    constructor(private _http: Http) { }
+
+    post(api: string, params: any) {
         let body = JSON.stringify(params);
         let headers = new Headers({ 'Content-Type': 'application/json' });
         let options = new RequestOptions({ headers: headers, method: "post" });
-        return this._http.post(this.global.serviceurl + api , body,options)
-            .map(res => res.json())
+        return this._http.post(this.global.serviceurl + api, body, options)
+            .map(res => {
+
+                console.log(res);
+                return res.json();
+            })
             .catch(this.handleError);
 
     }
 
-    get(method:string)
-    {
+
+
+    get(method: string) {
         // let headers = new Headers({ 'Content-Type': 'application/json' });
         // let options = new RequestOptions({ headers: headers, method: "get" });
         // return this._http.get(this.global.serviceurl ,options)
@@ -35,7 +39,7 @@ export class DataService {
     }
 
 
-    private handleError (error: Response) {
+    private handleError(error: Response) {
         console.error(error);
         return Observable.throw(error.json().error || ' error');
     }
