31038920bfb876003f695610ff97d6db333d295b
zaidansari <azaid51@gmail.com>
2017-02-04 00:40:24 -0800

    change input tag

--- src/app/_shared/sharedcomp.module.ts
+++ src/app/_shared/sharedcomp.module.ts
@@ -4,14 +4,15 @@ import { FileUploadComponent } from '../modules/usercontrol/fileupload/fileuploa
 import { SubMenuComponent } from '../modules/usercontrol/submenu/submenu.comp';
 import { AutoCompleteComponent } from '../modules/usercontrol/autocomplete/autocomplete.comp';
 import { AddrbookComp } from '../modules/usercontrol/addressbook/adrbook.comp';
+import { AttributeComp } from '../modules/usercontrol/attribute/attr.comp';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { FileUploadModule, GrowlModule, DialogModule } from 'primeng/primeng';
 
 @NgModule({
     imports: [RouterModule, FormsModule, CommonModule, FileUploadModule, GrowlModule, DialogModule],
-    declarations: [FileUploadComponent, SubMenuComponent, AutoCompleteComponent, AddrbookComp],
-    exports: [FileUploadComponent, SubMenuComponent, AutoCompleteComponent, AddrbookComp]
+    declarations: [FileUploadComponent, SubMenuComponent, AutoCompleteComponent, AddrbookComp, AttributeComp],
+    exports: [FileUploadComponent, SubMenuComponent, AutoCompleteComponent, AddrbookComp, AttributeComp]
 })
 
 export class SharedComponentModule { }
