c8f147be4a68fa6f77267b064b31d1465e16ecf0
vivekpandey12 <vivek.pandey5188@gmail.com>
2017-02-13 18:27:50 +0530

    make primeng datatable in user master

--- src/app/modules/setting/usermaster/view/viewuser.comp.ts
+++ src/app/modules/setting/usermaster/view/viewuser.comp.ts
@@ -2,9 +2,10 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SharedVariableService } from "../../../../_service/sharedvariable-service";
 import { ActionBtnProp } from '../../../../_model/action_buttons';
 import { Subscription } from 'rxjs/Subscription';
+import { LoginUserModel } from '../../../../_model/user_model';
 import { UserService } from '../../../../_service/user/user-service' /* add reference for view User */
-
 import { Router } from '@angular/router';
+import { LazyLoadEvent, DataTable } from 'primeng/primeng';
 
 @Component({
     templateUrl: 'viewUser.comp.html',
@@ -12,6 +13,10 @@ import { Router } from '@angular/router';
 })
 
 export class ViewUser implements OnInit, OnDestroy {
+    actionButton: ActionBtnProp[] = [];
+    subscr_actionbarevt: Subscription;
+    loginUser: LoginUserModel;
+
     type: string = "";
     value: string = "";
     name: string = "";
@@ -20,16 +25,20 @@ export class ViewUser implements OnInit, OnDestroy {
     EmpName: any;
     autoEmpName: any;
 
-    actionButton: ActionBtnProp[] = [];
-    subscr_actionbarevt: Subscription;
-
     viewUserDT: any[];
+    totalRecords: number = 0;
+    status: string = "";
+
+    statusDT: any = [];
 
     constructor(private _router: Router, private setActionButtons: SharedVariableService, private _userservice: UserService) {
-        this.getUserData();
+        this.loginUser = this._userservice.getUser();
+        this.fillStatusDropDown();
+        this.resetFYFields();
     }
 
     ngOnInit() {
+        this.setActionButtons.setTitle("Setting > User Master");
         this.title = "View User";
 
         this.actionButton.push(new ActionBtnProp("add", "Add", "plus", true, false));
@@ -37,19 +46,43 @@ export class ViewUser implements OnInit, OnDestroy {
         this.subscr_actionbarevt = this.setActionButtons.setActionButtonsEvent$.subscribe(evt => this.actionBarEvt(evt));
     }
 
-    selectedItem(e: any) {
-        console.log(this.autoEmpName);
-    }
+    fillStatusDropDown() {
+        var that = this;
 
-    getUserData() {
-        this._userservice.getUsers({ "flag": "" }).subscribe(data => {
-            this.viewUserDT = data.data;
+        this._userservice.getMenuDetails({
+            "flag": "trashrights", "ptype": "set", "mtype": "um",
+            "uid": 1, "cmpid": 1, "fy": 1
+        }).subscribe(data => {
+            that.statusDT = data.data;
         }, err => {
             console.log("Error");
         }, () => {
             // console.log("Complete");
+        });
+    }
+
+    resetFYFields() {
+        this.status = "true";
+    }
+
+    getUserData(from: number, to: number) {
+        this._userservice.getUsers({ "flag": "all", "from": from, "to": to, "isactive": this.status }).subscribe(users => {
+            this.totalRecords = users.data[1][0].recordstotal;
+            this.viewUserDT = users.data[0];
+        }, err => {
+            console.log(err);
+        }, () => {
+            // console.log("Complete");
         })
-    }    
+    }
+
+    loadUserGrid(event: LazyLoadEvent) {
+        this.getUserData(event.first, (event.first + event.rows));
+    }
+
+    searchUserData(dt: DataTable) {
+        dt.reset();
+    }
 
     openUserDetails(row) {
         this._router.navigate(['/setting/usermaster/edit', row.uid]);
