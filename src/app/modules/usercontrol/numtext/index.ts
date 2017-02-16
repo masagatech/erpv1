c4afef8383a501e4798027b7a019541164982aa6
pratikn <pratikway.90@gmail.com>
2017-02-16 16:17:22 +0530

    numtext change

--- src/app/modules/usercontrol/numtext/index.ts
+++ src/app/modules/usercontrol/numtext/index.ts
@@ -54,6 +54,9 @@ export class NumTextComp implements OnInit, ControlValueAccessor {
     @Input() min: string = "-9999999999999";
     @Input() max: string = "9999999999999";
     @Input() grpseperator: string = ",";
+    @Input() islabel: boolean = false;
+    @Input() tabindex: number = 500;
+
 
     decimalsArry: any = ["", ".9", ".99", ".999", ".9999", ".99999", ".999999", ".9999999"];
 
@@ -64,9 +67,9 @@ export class NumTextComp implements OnInit, ControlValueAccessor {
     private onTouchedCallback: () => void = noop;
     private onChangeCallback: (_: any) => void = noop;
 
-    get value(): any {
-        return this.innerValue;
-    };
+    // get value(): any {
+    //     return this.innerValue;
+    // };
 
     //set accessor including call the onchange callback
     set value(v: any) {
@@ -86,7 +89,7 @@ export class NumTextComp implements OnInit, ControlValueAccessor {
         decimalCharacterAlternative: this._userservice.getUser()._globsettings[0].thsep,
         currencySymbol: this._userservice.getUser()._globsettings[0].currsym,
         currencySymbolPlacement: this._userservice.getUser()._globsettings[0].currsymplace,
-        
+
         roundingMethod: 'U',
         minimumValue: "-9999999999999.99",
         maximumValue: "9999999999999.99"
@@ -102,6 +105,13 @@ export class NumTextComp implements OnInit, ControlValueAccessor {
     writeValue(value: any) {
         if (value !== this.innerValue) {
             this.innerValue = value;
+            var that = this;
+            setTimeout(function () {
+                $("#" + that.id + "," + "label_" + that.id).autoNumeric('set', value == "" ? 0 : value);
+            }, 10)
+            // if ($("#" + this.id).autoNumeric('init')) {
+            //     $("#" + this.id).autoNumeric('reSet');
+            // }
         }
     }
 
@@ -127,12 +137,12 @@ export class NumTextComp implements OnInit, ControlValueAccessor {
             that.autoNumericOptionsEuro.maximumValue = that.max + that.decimalsArry[that.decimals];
 
             if (that.iscurrency) {
-                $("#" + that.id).autoNumeric('init', that.autoNumericOptionsEuro);
+                $("#" + that.id + "," + "label_" + that.id).autoNumeric('init', that.autoNumericOptionsEuro);
             }
             else {
                 that.autoNumericOptionsEuro.digitGroupSeparator = that.grpseperator;
                 that.autoNumericOptionsEuro.currencySymbol = '';
-                $("#" + that.id).autoNumeric('init', that.autoNumericOptionsEuro);
+                $("#" + that.id + "," + "label_" + that.id).autoNumeric('init', that.autoNumericOptionsEuro);
             }
             that.updateModel();
         }, 100);
@@ -146,7 +156,7 @@ export class NumTextComp implements OnInit, ControlValueAccessor {
         var that = this;
 
         if ($("#" + that.id).autoNumeric()) {
-            var val = $("#" + that.id).autoNumeric('get');
+            var val = $("#" + that.id + "," + "label_" + that.id).autoNumeric('get');
             that.onChangeCallback(val == "" ? 0 : val);
         }
     }
