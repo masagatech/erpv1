4a567b07992c03c25e3b5f51843ed6f492f79af4
pratikn <pratikway.90@gmail.com>
2017-03-22 15:58:39 +0530

    currency pipe

--- src/app/_pipe/currency.pipe.ts
+++ src/app/_pipe/currency.pipe.ts
@@ -5,7 +5,15 @@ import { Pipe } from "@angular/core";
 })
 export class _currencyPipe {
   transform(money: any, args: any): any {
-    
-    return args.currsym +""+ parseFloat(money).toFixed(args.decimals).toString();
+
+    return args.currsym + "" + this.amt(money, args.decimals, 3, '.', ',');
+    // toFixed(args.decimals).toString();
+  }
+
+  amt(a, n, x, s, c) {
+    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\D' : '$') + ')',
+      num = a.toFixed(Math.max(0, ~~n));
+
+    return (c ? num.replace('.', c) : num).replace(new RegExp(re, 'g'), '$&' + (s || ','));
   }
 }
--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -52,11 +52,3 @@ var browserConf = {};
 browserConf.setTitle = function(title) {
     document.title = title;
 }
-
-
-Number.prototype.format = function(n, x, s, c) {
-    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\D' : '$') + ')',
-        num = this.toFixed(Math.max(0, ~~n));
-
-    return (c ? num.replace('.', c) : num).replace(new RegExp(re, 'g'), '$&' + (s || ','));
-};
