303813f19761e6d64237009dec638be3b958b206
pratikn <pratikway.90@gmail.com>
2017-03-22 16:38:21 +0530

    currency pipe

--- src/app/_pipe/currency.pipe.ts
+++ src/app/_pipe/currency.pipe.ts
@@ -6,13 +6,13 @@ import { Pipe } from "@angular/core";
 export class _currencyPipe {
   transform(money: any, args: any): any {
 
-    return args.currsym + "" + this.amt(money, args.decimals, 3, '.', ',');
+    return args.currsym + "" + this.amt(money, args.decimals, 3, ',', '.');
     // toFixed(args.decimals).toString();
   }
 
   amt(a, n, x, s, c) {
     var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\D' : '$') + ')',
-      num = a.toFixed(Math.max(0, ~~n));
+      num = parseFloat(a).toFixed(Math.max(0, ~~n));
 
     return (c ? num.replace('.', c) : num).replace(new RegExp(re, 'g'), '$&' + (s || ','));
   }
